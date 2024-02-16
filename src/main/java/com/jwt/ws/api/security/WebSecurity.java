78e3166a61cb383cb8a6f29e605cb204d003609b
Mukesh Kumar <mukesh.kumar@amantyatech.com>
2024-02-16 12:54:11 +0530

    with role upgraded to spring boot 3.2.1

--- src/main/java/com/jwt/ws/api/security/WebSecurity.java
+++ src/main/java/com/jwt/ws/api/security/WebSecurity.java
@@ -1,26 +1,33 @@
 package com.jwt.ws.api.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
+public class WebSecurity {
+	
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		
 		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
 		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
-
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/status/check")
+	 	
+		http 
+		.authorizeHttpRequests((authz) -> authz.requestMatchers(HttpMethod.GET, "/users/status/check")
 				// .hasAuthority("SCOPE_profile")
 				.hasRole("developer")
-				// .hasAnyRole("devleoper","user")
-				.anyRequest().authenticated().and().oauth2ResourceServer().jwt()
-				.jwtAuthenticationConverter(jwtAuthenticationConverter);
+				// .hasAnyAuthority("ROLE_developer")
+				.anyRequest().authenticated())
+		.oauth2ResourceServer(
+				oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+		
+		return http.build();
 	}
-
+	
 }
