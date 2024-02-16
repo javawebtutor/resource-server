a1646a83ee9326afd8ee274fef150de959e423f5
Mukesh Kumar <mukesh.kumar@amantyatech.com>
2024-02-16 12:09:18 +0530

    adding method level security

--- src/main/java/com/jwt/ws/api/security/WebSecurity.java
+++ src/main/java/com/jwt/ws/api/security/WebSecurity.java
@@ -1,28 +1,31 @@
 package com.jwt.ws.api.security;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled = true)
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebSecurity {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
 		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
 		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/status/check")
+		http.authorizeHttpRequests((authz) -> authz.requestMatchers(HttpMethod.GET, "/users/status/check")
 				// .hasAuthority("SCOPE_profile")
-				//.hasRole("developer")
-		        .hasAuthority("ROLE_developer")
-				.anyRequest().authenticated().and().oauth2ResourceServer().jwt()
-				.jwtAuthenticationConverter(jwtAuthenticationConverter);
+				.hasRole("developer")
+				// .hasAnyAuthority("ROLE_developer")
+				.anyRequest().authenticated()).oauth2ResourceServer(
+						oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+		return http.build();
 	}
 
 }
